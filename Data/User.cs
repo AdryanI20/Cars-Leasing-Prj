50dfc3170d627d7268bde792aac59ea2cd40bf7f
AdryanI20 <gameder14@gmail.com>
2025-10-27 17:27:09 +0200

    Setup Data Structure

---
+++ Data/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoLeasingApp.Data
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(255)]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public List<LeaseContract> LeaseContracts { get; set; } = new List<LeaseContract>();
+    }
+}
