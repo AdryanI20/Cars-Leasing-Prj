90dd2eb6b1f05452a1bc2b947cd932040fcc5ed1
AdryanI20 <gameder14@gmail.com>
2025-10-27 21:49:08 +0200

    Setup Nuget Packages and Database

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 using System.Windows;
 
 namespace ProiectPractica
@@ -9,6 +11,18 @@ namespace ProiectPractica
     /// </summary>
     public partial class App : Application
     {
+        private IConfiguration? _configuration;
+
+        public App()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true);
+            _configuration = builder.Build();
+
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            System.Diagnostics.Debug.WriteLine($"Connection String: {_configuration.GetConnectionString("DefaultConnection")}");
+        }
     }
 
 }
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoLeasingApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) // Accept options from DI
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<LeaseContract> LeaseContracts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<LeaseContract>()
+                .HasOne(lc => lc.User)
+                .WithMany(u => u.LeaseContracts)
+                .HasForeignKey(lc => lc.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<LeaseContract>()
+                .HasOne(lc => lc.Car)
+                .WithMany(c => c.LeaseContracts)
+                .HasForeignKey(lc => lc.CarId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Car>()
+                .Property(c => c.IsAvailable)
+                .HasDefaultValue(true);
+
+            modelBuilder.Entity<Car>()
+                .Property(c => c.DailyRate)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<LeaseContract>()
+                .Property(lc => lc.TotalCost)
+                .HasPrecision(18, 2);
+        }
+    }
+}
